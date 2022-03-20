4ab82bab05c2d85061101574072e0fba3cb557de
duoduo <wxjlmr@126.com>
2022-03-20 10:49:04 +0800

    findlist

--- test.sh
+++ test.sh
@@ -4,14 +4,28 @@
 #
 #
 
-dos2unix $0
+dos2unix $0 >/dev/null 2>1&
 
 source ./public.sh
 
 
 
 ########main proc
+# 显示帮助
+[ $# -lt 0 ] && usage "./eg.sh "
+#[ $# -lt 1 ] && usage "./eg.sh RootPath"
 checkdirnoexistquit ./
 checkdirnoexistquit abc
 checkfilenoexistquit public.sh
-checkfilenoexistquit public2.sh
+checkfilenoexistquit test.sh
+#checkfilenoexistquit test1.sh
+
+lsinfo .
+#lsinfo ..
+#lsinfo ~
+
+
+findlistall .
+findlistignore . ".git\|.sh$"
+findlistuse . .git
+
